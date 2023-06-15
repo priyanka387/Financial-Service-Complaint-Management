188ad22ae11317386cc826fcbe5cbb7829146faa
priyanka387 <priyankapathak222@gmail.com>
2023-06-15 11:40:28 +0530

    intitial commit

---
+++ finance_complaint/constant/environment/variable_key.py
@@ -0,0 +1,4 @@
+AWS_ACCESS_KEY_ID = "AKIAQ4JPEPN7PRZWGFHL"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_DEFAULT_REGION="ap-south-1"
+MONGO_DB_URL = "mongodb+srv://priyanka:priya123@cluster0.wbh31ji.mongodb.net/?retryWrites=true&w=majority"
